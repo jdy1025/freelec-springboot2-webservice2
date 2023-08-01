5931370a36eae6c82d898944101eed785c03efa2
전다영 <jeondayoung@jeondayeong-ui-MacBookPro.local>
2023-08-01 20:13:38 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice2/FreelecSpringboot2Webservice2Application.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2webservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2Webservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringboot2Webservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice2/FreelecSpringboot2Webservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2webservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2Webservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
